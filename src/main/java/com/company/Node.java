0548f807b371567beb094c008a77e3b70cf8e2c8
Bashar <salmobeschir@gmail.com>
2026-07-24 15:58:34 +0200

    SETUP Node class

--- src/main/java/com/company/Node.java
+++ src/main/java/com/company/Node.java
@@ -1,4 +1,38 @@
 package com.company;
 
-public class Node  extends ListItem {
+public class Node extends ListItem {
+
+    public Node(Object value) {
+        super(value);
+    }
+
+    @Override
+    ListItem next() {
+        return this.rightLink;
+    }
+
+    @Override
+    ListItem setNext(ListItem next) {
+        this.rightLink = next;
+        return this.rightLink;
+    }
+
+    @Override
+    ListItem previous() {
+        return this.leftLink;
+    }
+
+    @Override
+    ListItem setPrevious(ListItem previous) {
+        this.leftLink = previous;
+        return this.leftLink;
+    }
+
+    @Override
+    int compareTo(ListItem other) {
+        Integer obj1 = (Integer) this.getValue();
+        Integer obj2 = (Integer) other.getValue();
+
+        return obj1.compareTo(obj2);
+    }
 }
