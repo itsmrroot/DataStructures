b39df73a83023e6838ef3cd1996b6948a8753b19
Bashar <salmobeschir@gmail.com>
2026-07-24 15:28:19 +0200

    SETUP ListItem class

--- src/main/java/com/company/ListItem.java
+++ src/main/java/com/company/ListItem.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public  abstract class ListItem {
+    protected  ListItem rightLink;
+    protected  ListItem leftLink;
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+
+     abstract ListItem next();
+     abstract void setNext(ListItem item);
+
+     abstract ListItem previous();
+     abstract void setPrevious(ListItem item);
+
+     abstract int compareTo(Object other);
+
+    public Object getValue() { return value; }
+
+    public void setValue(Object value) { this.value = value; }
+
+
 }
